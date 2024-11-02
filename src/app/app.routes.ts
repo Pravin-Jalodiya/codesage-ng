ae99d2b9f2058fd437621f72abc1c62e26da1d5a
Pravin <vkvk3113@gmail.com>
2024-11-02 23:30:54 +0530

    added landing page

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,10 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+import {LandingPageComponent} from "./components/landing-page/landing-page.component";
+import {AuthFormComponent} from "./components/auth-form/auth-form.component";
+
+export const routes: Routes = [
+  { path: '', component: LandingPageComponent },
+  { path: 'login', component: AuthFormComponent },
+  { path: 'signup', component: AuthFormComponent }
+];
