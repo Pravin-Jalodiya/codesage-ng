2d2be71121623cfe7f6a05309e50433b4dac4911
Pravin Jalodiya <Pravin.Jalodiya@watchguard.com>
2024-11-06 14:07:52 +0530

    added user profile page

--- src/app/components/profile/profile.component.ts
+++ src/app/components/profile/profile.component.ts
@@ -1,5 +1,7 @@
 import { Component } from '@angular/core';
-import {FormGroup} from "@angular/forms";
+import {AbstractControlOptions, FormBuilder, FormControl, FormGroup, Validators} from "@angular/forms";
+import {AuthService} from "../../shared/services/auth/auth.service";
+import {MessageService} from "primeng/api";
 
 @Component({
   selector: 'app-profile',
@@ -8,5 +10,18 @@ import {FormGroup} from "@angular/forms";
 })
 
 export class ProfileComponent {
-  profileForm: FormGroup<any> | undefined;
+  profileForm: FormGroup;
+
+  constructor(private fb: FormBuilder, private authService: AuthService, private messageService: MessageService) {
+    this.profileForm = this.fb.group({
+      username: ['', [Validators.required, Validators.minLength(3)]],
+      fullname: ['', [Validators.required]],
+      password: ['', [Validators.required, Validators.minLength(6)]],
+      confirmPassword: ['', [Validators.required]],
+      email: ['', [Validators.required, Validators.email]],
+      leetcodeId: ['', [Validators.required]],
+      country: ['', [Validators.required]],
+      organisation: ['', [Validators.required]],
+    });
+  }
 }
